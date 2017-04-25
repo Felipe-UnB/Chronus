5d90977601820943a24d1e1abd4abd769f16cc44
Felipe-UnB <felipe.valencaa@yahoo.com.br>
2017-04-24 23:32:46 -0300

    Setup created

---
+++ Software/Python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Chronus',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Felipe-UnB/Chronus/wiki',
+    license='',
+    author='Felipe Valença',
+    author_email='felipe.valencaa@yahoo.com.br',
+    description='Software to plot U-Pb data and prepare it so Chronus can handle it'
+)
